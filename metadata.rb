084b20e428092f9c40827d92e66f58441ec4caca
Josh McDade <josh.ncsu@gmail.com>
2014-08-20 07:53:19 -0700

    0.1.5 version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'josh.ncsu@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures rails_stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'apt'
 depends 'nginx'
