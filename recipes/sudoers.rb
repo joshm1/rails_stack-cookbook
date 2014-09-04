d58d3a022e4c1770d0f92b8fafe76cf8507c06f5
Josh McDade <josh.ncsu@gmail.com>
2014-09-04 07:27:29 -0700

    add resque-scheduler to sudoers file

--- recipes/sudoers.rb
+++ recipes/sudoers.rb
@@ -2,6 +2,7 @@ rails_apps.each do |app|
   nopasswds = []
   nopasswds << app.app_server.init_file if app.app_server.run?
   nopasswds << app.resque.init_file if app.resque.pool?
+  nopasswds << app.resque_scheduler.init_file if app.resque_scheduler.enabled?
 
   template "/etc/sudoers.d/#{app.full_name}" do
     variables :app => app, :nopasswd_files => nopasswds.join(', ')
